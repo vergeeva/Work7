5ae83674e18ee8ef06d8ac2ca6e25b7a2cbd76b6
nastya <nastya.vergeeva@mail.ru>
2021-03-09 23:21:04 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.vb
@@ -0,0 +1,174 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TabPage3 = New System.Windows.Forms.TabPage()
+        Me.TabPage4 = New System.Windows.Forms.TabPage()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
+        Me.TabPage3.SuspendLayout()
+        Me.TabPage4.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TabControl1
+        '
+        Me.TabControl1.AllowDrop = True
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Controls.Add(Me.TabPage3)
+        Me.TabControl1.Controls.Add(Me.TabPage4)
+        Me.TabControl1.Location = New System.Drawing.Point(12, 12)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(383, 369)
+        Me.TabControl1.TabIndex = 0
+        '
+        'TabPage1
+        '
+        Me.TabPage1.AllowDrop = True
+        Me.TabPage1.Controls.Add(Me.Button1)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 25)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(375, 340)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Первое"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.AllowDrop = True
+        Me.Button1.Location = New System.Drawing.Point(106, 123)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(157, 34)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Перетащи меня"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Controls.Add(Me.TextBox2)
+        Me.TabPage2.Controls.Add(Me.TextBox1)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 25)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(375, 340)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "Второе"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.AllowDrop = True
+        Me.TextBox2.Location = New System.Drawing.Point(37, 131)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(230, 22)
+        Me.TextBox2.TabIndex = 1
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(37, 44)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(230, 22)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TabPage3
+        '
+        Me.TabPage3.Controls.Add(Me.TextBox3)
+        Me.TabPage3.Location = New System.Drawing.Point(4, 25)
+        Me.TabPage3.Name = "TabPage3"
+        Me.TabPage3.Size = New System.Drawing.Size(375, 340)
+        Me.TabPage3.TabIndex = 2
+        Me.TabPage3.Text = "Третье"
+        Me.TabPage3.UseVisualStyleBackColor = True
+        '
+        'TabPage4
+        '
+        Me.TabPage4.AllowDrop = True
+        Me.TabPage4.Controls.Add(Me.RichTextBox1)
+        Me.TabPage4.Location = New System.Drawing.Point(4, 25)
+        Me.TabPage4.Name = "TabPage4"
+        Me.TabPage4.Size = New System.Drawing.Size(375, 340)
+        Me.TabPage4.TabIndex = 3
+        Me.TabPage4.Text = "Четвертое"
+        Me.TabPage4.UseVisualStyleBackColor = True
+        '
+        'TextBox3
+        '
+        Me.TextBox3.AllowDrop = True
+        Me.TextBox3.Location = New System.Drawing.Point(89, 133)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(185, 20)
+        Me.TextBox3.TabIndex = 0
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.EnableAutoDragDrop = True
+        Me.RichTextBox1.Location = New System.Drawing.Point(47, 55)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(254, 152)
+        Me.RichTextBox1.TabIndex = 0
+        Me.RichTextBox1.Text = ""
+        '
+        'Form1
+        '
+        Me.AllowDrop = True
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(416, 393)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Лабораторная работа №7"
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage2.ResumeLayout(False)
+        Me.TabPage2.PerformLayout()
+        Me.TabPage3.ResumeLayout(False)
+        Me.TabPage3.PerformLayout()
+        Me.TabPage4.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents TabPage3 As TabPage
+    Friend WithEvents TabPage4 As TabPage
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents RichTextBox1 As RichTextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,67 @@
+﻿Public Class Form1
+    Private Sub Button1_MouseDown(sender As Object, e As MouseEventArgs) Handles Button1.MouseDown
+        Me.Button1.DoDragDrop(Me.Button1, DragDropEffects.Move)
+        Cursor.Current = Cursors.Hand
+
+    End Sub
+
+    Private Sub TabPage1_DragOver(sender As Object, e As DragEventArgs) Handles TabPage1.DragOver
+        Me.Button1.Location = Me.PointToClient(New Point(e.X, e.Y))
+        Cursor.Current = Cursors.Hand
+    End Sub
+
+    Private Sub TextBox1_MouseDown(sender As Object, e As MouseEventArgs) Handles TextBox1.MouseDown
+        Me.TextBox1.DoDragDrop(Me.TextBox1.Text, DragDropEffects.Copy)
+    End Sub
+
+    Private Sub TextBox2_DragEnter(sender As Object, e As DragEventArgs) Handles TextBox2.DragEnter
+        e.Effect = DragDropEffects.Copy
+    End Sub
+
+    Private Sub TextBox2_DragDrop(sender As Object, e As DragEventArgs) Handles TextBox2.DragDrop
+        TextBox2.AppendText(e.Data.GetData(DataFormats.Text).ToString)
+    End Sub
+
+    Private Sub Form1_DragEnter(sender As Object, e As DragEventArgs) Handles MyBase.DragEnter
+
+    End Sub
+
+    Private Sub TextBox3_DragEnter(sender As Object, e As DragEventArgs) Handles TextBox3.DragEnter
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then e.Effect = DragDropEffects.Copy
+    End Sub
+
+    Private Sub TextBox3_DragDrop(sender As Object, e As DragEventArgs) Handles TextBox3.DragDrop
+        Dim mas() As String = e.Data.GetData(DataFormats.FileDrop, True)
+        Dim j As Integer
+        Dim fd As Integer
+        Dim str As String = ""
+        For j = 0 To UBound(mas)
+            fd = FreeFile()
+            FileOpen(fd, mas(j), OpenMode.Input, OpenAccess.Read, OpenShare.LockReadWrite)
+            While Not EOF(fd)
+                str = str & LineInput(fd) & vbCrLf
+            End While
+            FileClose(fd)
+            str &= vbCrLf
+        Next
+        Me.TextBox3.Text = str
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        RichTextBox1.AllowDrop = True
+    End Sub
+
+    Private Sub TabPage4_DragEnter(sender As Object, e As DragEventArgs) Handles TabPage4.DragEnter
+        If e.Data.GetDataPresent(DataFormats.Rtf) Then e.Effect = DragDropEffects.Copy
+    End Sub
+
+    Private Sub TabPage4_DragDrop(sender As Object, e As DragEventArgs) Handles TabPage4.DragDrop
+        Me.RichTextBox1.Text = Me.RichTextBox1.Text & vbCrLf & e.Data.GetData(DataFormats.Text).ToString
+    End Sub
+
+    Private Sub RichTextBox1_MouseDown(sender As Object, e As MouseEventArgs) Handles RichTextBox1.MouseDown
+        If e.Button = Windows.Forms.MouseButtons.Left Then
+            Me.RichTextBox1.DoDragDrop(Me.RichTextBox1.SelectedText, DragDropEffects.Copy)
+        End If
+    End Sub
+End Class
